884483cb3d720fb951b4ae98c068b8cb9138a0d9
Yashodhar Karki <yashodhar.karkii@gmail.com>
2024-10-22 07:04:45 +0530

    Initial commit

---
+++ conftest.py
@@ -0,0 +1,22 @@
+from selenium.webdriver.chrome.options import Options
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(scope="session")
+def browser_options():
+    options = Options()
+    # options.add_argument("--headless")
+    # options.add_argument("--incognito")  # runs chrome in incognito mode
+    options.add_argument("--start-maximized")  # Start window maximized
+    options.add_argument("--no-sandbox")
+    # options.add_argument("--window-size=900,600")  # window size customization
+    # options.add_argument("--disable-infobars")
+    return options
+
+
+@pytest.fixture(scope="session")
+def launch_chrome():
+    driver = webdriver.Chrome()
+    driver.get("https://app.vwo.com/#/login")
+    return driver
